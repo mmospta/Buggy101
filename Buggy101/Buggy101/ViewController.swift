160e138f9a8ae4e2939b60762a89c812f17d3233
Phonthep Aungkanukulwit <mos11134@gmail.com>
2019-09-16 22:58:36 +0700

    [INIT] Initial Buggy101

---
+++ Buggy101/Buggy101/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Buggy101
+//
+//  Created by Phonthep Aungkanukulwit on 16/9/2562 BE.
+//  Copyright © 2562 Phonthep Aungkanukulwit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
